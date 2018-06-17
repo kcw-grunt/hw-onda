3eb6f316a5199840827c1add80f54b3afab11b84
Kerry <mrkerrywashington@icloud.com>
2018-06-16 22:31:23 -0700

    add local ax25

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3001;
-const ax25 = require('ax25');
+const ax25 = require('./src/ax25');
 const tnc = new ax25.kissTNC({ serialPort : "/dev/ttyUSB0", baudRate : 9600 } );
 tnc.on('frame', (frame) => { let packet = new ax25.Packet({ frame : frame }); console.log(`src : ${packet.sourceCallsign}, dst : ${packet.destinationCallsign}, inf : ${packet.infoString}`; });
 tnc.on('error', (err) => console.log('error' + err));
